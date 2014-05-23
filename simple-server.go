296f756d895be40c21a8e042ce8cfc6f7d209056
Alberto Grespan <albertogrespan@gmail.com>
2014-05-23 18:40:03 -0430

    Add flag for port

--- simple-server.go
+++ simple-server.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+var (
+	port int
+)
+
 func rootHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Printf("%s %s %s\n", req.RemoteAddr, req.Method, req.URL)
 	fmt.Fprint(w, "Hello World!")
 }
 
+func init() {
+	flag.IntVar(&port, "port", 4000, "HTTP Server Port")
+	flag.Parse()
+}
+
 func main() {
+	httpAddr := fmt.Sprintf(":%v", port)
+	log.Printf("Listening to %v", httpAddr)
+
 	http.HandleFunc("/", rootHandler)
-	log.Printf("Listening to 3000")
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	log.Fatal(http.ListenAndServe(httpAddr, nil))
 }
